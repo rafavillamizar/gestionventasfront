2fcfdcd50fb1482abce61add6773f63a0c7664e0
Rafael Villamizar <rafavillamizar@gmail.com>
2016-01-09 00:04:56 +0100

    Gestion producto

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -2,47 +2,126 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view3', {
+            templateUrl: 'view3/view3.html',
+            controller: 'View3Ctrl'
+        });
+    }])
 
-.controller('View3Ctrl', ['$scope', '$http', function($scope, $http) {
-      //$scope.formData = {};
+    .controller('View3Ctrl', ['$scope', '$http', '$uibModal', function ($scope, $http, $uibModal) {
+        $scope.producto = {};
 
-      $http.get('http://localhost:8080/gestionventas/productos')
-          .success(function(data) {
-            $scope.productos = data;
-            console.log(data)
-          })
-          .error(function(data) {
-            console.log('Error: ' + data);
-          });
+        $scope.prepararNuevoProducto = function () {
+            $scope.producto = {
+                nombre: "",
+                precio: ""
+            };
+            $scope.abrirDetalleProducto();
+        }
 
-      // Cuando se añade un nuevo TODO, manda el texto a la API
-      //$scope.createTodo = function(){
-      //  $http.post('http://localhost:8080/gestionventas/ventas', $scope.formData)
-      //      .success(function(data) {
-      //        $scope.formData = {};
-      //        $scope.todos = data;
-      //        console.log(data);
-      //      })
-      //      .error(function(data) {
-      //        console.log('Error:' + data);
-      //      });
-      //};
+        $scope.prepararEdicionProducto = function (producto) {
+            $scope.producto = producto;
+            $scope.abrirDetalleProducto();
+        }
 
-      // Borra un TODO despues de checkearlo como acabado
-      //$scope.deleteTodo = function(id) {
-      //  $http.delete('http://localhost:8080/gestionventas/ventas/' + id)
-      //      .success(function(data) {
-      //        $scope.todos = data;
-      //        console.log(data);
-      //      })
-      //      .error(function(data) {
-      //        console.log('Error:' + data);
-      //      });
-      //};
-}]);
+        $scope.prepararEliminarProducto = function (producto) {
+            $scope.producto = producto;
+            $scope.abrirEliminarProducto();
+        }
+
+        $scope.obtenerProductos = function () {
+            $http.get('http://192.168.1.136:8080/gestionventas/productos')
+                .success(function (data) {
+                    $scope.productos = data;
+                    console.log(data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });
+        };
+
+        $scope.guardarProducto = function (producto) {
+            $http.post('http://192.168.1.136:8080/gestionventas/productos', producto)
+                .success(function (data) {
+                    $scope.obtenerProductos();
+                })
+                .error(function (data) {
+                    console.log('Error:' + data);
+                });
+        };
+
+        $scope.eliminarProducto = function(productoId) {
+          $http.delete('http://192.168.1.136:8080/gestionventas/productos/' + productoId)
+              .success(function(data) {
+                  $scope.obtenerProductos();
+              })
+              .error(function(data) {
+                console.log('Error:' + data);
+              });
+        };
+
+        $scope.abrirDetalleProducto = function () {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'nuevoProductoModalContent.html',
+                controller: 'NuevoProductoModalInstanceCtrl',
+                size: 'lg',
+                resolve: {
+                    producto: function () {
+                        return $scope.producto;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (producto) {
+                $scope.guardarProducto(producto);
+            });
+        };
+
+        $scope.abrirEliminarProducto = function () {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'eliminarProductoModalContent.html',
+                controller: 'EliminarProductoModalInstanceCtrl',
+                size: 'sm',
+                resolve: {
+                    producto: function () {
+                        return $scope.producto;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (producto) {
+                $scope.eliminarProducto(producto.productoId);
+            });
+        };
+
+        $scope.obtenerProductos();
+    }])
+
+    .controller('NuevoProductoModalInstanceCtrl', ['$scope', '$uibModalInstance', 'producto', function ($scope, $uibModalInstance, producto) {
+        $scope.producto = producto;
+
+        $scope.guardar = function () {
+            $uibModalInstance.close($scope.producto);
+        };
+
+        $scope.cancelar = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }])
+
+    .controller('EliminarProductoModalInstanceCtrl', ['$scope', '$uibModalInstance', 'producto', function ($scope, $uibModalInstance, producto) {
+        $scope.producto = producto;
+
+        $scope.ok = function () {
+            $uibModalInstance.close($scope.producto);
+        };
+
+        $scope.cancelar = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    }]);
